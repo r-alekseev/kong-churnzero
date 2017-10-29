c8d8586b60e4a35b9394cd4b81f554311285dc6c
Roman Alekseev <r-alekseev@tuta.io>
2017-10-29 21:07:50 +0400

    feat(settings) basic settings

--- kong/plugins/churnzero/schema.lua
+++ kong/plugins/churnzero/schema.lua
@@ -1,11 +1,18 @@
+local utils = require "kong.tools.utils"
+
 return {
-  no_consumer = false, -- this plugin is available on APIs as well as on Consumers,
+  no_consumer = false,
   fields = {
-    -- Describe your plugin's configuration's schema here.
-    
+    endpoint_url = { required = true, type = "url" },
+    timeout = { default = 10000, type = "number" },
+    keepalive = { default = 60000, type = "number" },
+    app_key = { required = true, type = "string" },
+    account_external_id = { required = true, type = "string" },
+    contact_external_id_authenticated_only = { default = true, type = "bool" },
+    event_from_response_header_names = { default = "X-ChurnZero-Event", type = "string" },
+    event_name_quantity_separator = { default = ":", type = "string" }
   },
   self_check = function(schema, plugin_t, dao, is_updating)
-    -- perform any custom verification
     return true
   end
 }
